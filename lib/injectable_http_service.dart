6779b6125348eddacf652a9ce5df7b38be542784
Nikita Kozlov <nikita@thetourguy.com>
2022-10-29 12:18:35 +0300

    init

--- lib/injectable_http_service.dart
+++ lib/injectable_http_service.dart
@@ -1,6 +1,7 @@
 library injectable_http_service;
 
 export 'src/contracts/http_service.dart';
+export 'src/base/http_service_base.dart';
 export 'src/json_http_service_impl.dart';
 export 'src/models/app_http_request.dart';
 export 'src/constants/http_verb.enum.dart';
